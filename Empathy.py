e87da79d74dae635c33ec3ae1f3158ff241764f2
Jezabeel <jezabeeel@gmail.com>
2017-08-11 18:30:21 +0200

    screen changer moved to separate file

--- Empathy.py
+++ Empathy.py
@@ -1,25 +1,16 @@
 #kivy.require("1.10.0")
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.core.window import Window, WindowBase
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.lang.builder import Builder
 # noinspection PyUnresolvedReferences
-from gamescreen import GameScreen
-# noinspection PyUnresolvedReferences
-from mainmenuscreen import MainMenuScreen
-# noinspection PyUnresolvedReferences
-from optionsscreen import OptionsScreen
+from screenchanger import ScreenChanger
 
 Window.size = (600,800)
 Window.minimum_width = 600
 Window.minimum_height = 800
 #Window.fullscreen = 'auto'
 #Window.clearcolor = (0.03,0.07,0.01,1)
-
-
-class ScreenChanger(ScreenManager):
-	pass
 
 class Empathy(App):
 
@@ -28,9 +19,10 @@ class Empathy(App):
 	def build(self):
 		self.title = 'Empathy'
 		masterWidget = BoxLayout()
-		masterWidget.add_widget(ScreenChanger())
+		screenChanger = Builder.load_file("screenchanger.kv")
+		masterWidget.add_widget(screenChanger)
 		masterWidget.size = Window.size
 		return masterWidget
 
 if __name__=="__main__":
-	Empathy().run() 
+	Empathy().run()
---
+++ screenchanger.py
@@ -0,0 +1,12 @@
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+# noinspection PyUnresolvedReferences
+from mainmenuscreen import MainMenuScreen
+# noinspection PyUnresolvedReferences
+from gamescreen import GameScreen
+# noinspection PyUnresolvedReferences
+from optionsscreen import OptionsScreen
+
+
+class ScreenChanger(ScreenManager):
+	pass
