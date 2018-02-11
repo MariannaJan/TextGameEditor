3e12e521cb41640d6557963b6ad4a9959ded5270
Jezabeel <jezabeeel@gmail.com>
2018-02-11 11:20:04 +0100

    separated Strings file

--- gamestrings.py
+++ gamestrings.py
@@ -9,3 +9,17 @@ class GameStrings:
     newgameconfirmtext = 'Do you want to start a new game? All progress will be lost!!!'
     yestext = 'Yes'
     notext = 'No'
+    inspecttext = "Inspect"
+    interacttext = "Interact"
+    inventorytext = "Inventory"
+    interfacetext = "Interface"
+    closetext = "Close"
+    ininventorytext = 'Already in the inventory!'
+    gotoinventorytext = 'Go to Inventory'
+    availablelocationstext = 'Available Locations'
+    journaltext = 'Journal'
+    availablestoriestext = 'Available Stories'
+    emptytext = 'Empty!'
+    themetext = "Theme"
+    soundtext = "Sound"
+    soundvolumetext = 'Sound volume:'
